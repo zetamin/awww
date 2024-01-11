777eccdbffee8f4ea3384e36bcd844a9d3c6f5f5
zetamin <140958647+zetamin@users.noreply.github.com>
2024-01-11 14:51:07 +0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start Docker daemon if not running
-sudo service docker status || sudo service docker start
+service docker status || service docker start
 
 # Start Portainer container if not running
-sudo docker ps -a --format '{{.Names}}' | grep -q '^portainer$' || sudo docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /data/portainer_data:/data --name portainer portainer/portainer
+docker ps -a --format '{{.Names}}' | grep -q '^portainer$' || docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /data/portainer_data:/data --name portainer portainer/portainer
 
 # Keep the script running
 tail -f /dev/null
